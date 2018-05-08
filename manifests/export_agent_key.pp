5e35d37e9fc66fb2fba3ff2c7dfbd767e841aaf8
ajurjevi <26479807+ajurjevi@users.noreply.github.com>
2018-05-08 09:50:12 +0200

    Add seed for more randomness for agnet_id in manage_client_keys option (#8)

--- manifests/export_agent_key.pp
+++ manifests/export_agent_key.pp
@@ -6,14 +6,14 @@ class wazuh::export_agent_key(
   $agent_seed,
   ) {
   wazuh::agentkey{ "ossec_agent_${agent_name}_client":
-    agent_id         => fqdn_rand($max_clients),
+    agent_id         => fqdn_rand($max_clients, $agent_ip_address),
     agent_name       => $agent_name,
     agent_ip_address => $agent_ip_address,
     agent_seed       => $agent_seed,
   }
 
   @@wazuh::agentkey{ "ossec_agent_${agent_name}_server":
-    agent_id         => fqdn_rand($max_clients),
+    agent_id         => fqdn_rand($max_clients, $agent_ip_address),
     agent_name       => $agent_name,
     agent_ip_address => $agent_ip_address,
     agent_seed       => $agent_seed,
